fcd625974e76603c1202414c86513bcaf1ae5fa4
lucasigna <lucasignaciogonzalez1999@gmail.com>
2022-02-19 11:59:50 -0300

    Empezando menú

---
+++ src/components/NavBar/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Precios</li>
+                <li>Coins</li>
+                <li>Contacto</li>
+                <li>API</li>
+            </ul>
+        </nav>
+    )
+}
